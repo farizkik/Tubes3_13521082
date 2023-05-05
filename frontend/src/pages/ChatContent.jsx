388980a74654328ebb42e4504da36a9b3d903d03
farizkik <fariski.mail@gmail.com>
2023-05-05 21:53:23 +0700

    feat: new ui look

--- frontend/src/pages/ChatContent.jsx
+++ frontend/src/pages/ChatContent.jsx
@@ -2,8 +2,24 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom';
 import './Home.css'
+import ReactScrollableFeed from 'react-scrollable-feed'
+import {Link} from 'react-router-dom'
 
 const ChatContent = () => {
+    const [sidebar,setSidebar] = useState(false)
+
+    const showSidebar = () => setSidebar(!sidebar)
+    const [convos, setConvo] = useState([]);
+
+    useEffect(()=>{
+        getConvos();
+    },[]);
+    
+    const getConvos = async()=>{
+        const response = await axios.get('http://localhost:5000/api/histories/');
+        setConvo(response.data.reverse());
+    };
+
     const [Text, setText] = useState("");
     const [Sender, setSender] = useState("");
     const [Bubble, setBubble] = useState("");
@@ -13,6 +29,9 @@ const ChatContent = () => {
 
     const [chats, setChat] = useState([]);
 
+    function keyDown(val){
+        if(val.keyCode===13 && !val.shiftKey)document.getElementById("btnSend").click();
+    }
     useEffect(()=>{
         getChats();
     },[]);
@@ -52,6 +71,7 @@ const ChatContent = () => {
                 });
 
                 getChats()
+                setText("")
             } catch (error) {
                 console.log(error);
             }
@@ -59,35 +79,67 @@ const ChatContent = () => {
     };
 
     return (
-        <div className="center">
-        <div className="columns mt-5 is-centered">
-            <div className="column is-half">
+        <div className="center isi App">
+        
+        <aside className='sidemenu'>
+            <div className='new-chat' onClick={() => {window.location.href="/"}}>
+                <span>+</span>
+                New Chat
+            </div>
+            <select name="cars" id="cars" className='algo-select'>
+                <option value="KMP" className='algo-item'>Knuth Morris Path</option>
+                <option value="BM" className='algo-item'>Bayer-Moore</option>
+            </select>
+            <div className="scroll-convos">   
+                {convos.map((chat,index) => {
+                    return(
+                        <li key={index+10} className='nav-text'>
+                                <Link to={'../convo/' + chat.ChatId}>
+                                    <span>
+                                        {chat.Text}
+                                    </span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+            </div>
+
+        </aside>
+
+        <section className='contentbox'>
+
+
+        <div className="chat-log">
+            <div className="chat-message">
+        <ReactScrollableFeed className='width-changer'>
                 {chats.map((chat, index) =>(
-                    <div className={chat.Sender}> {chat.Text} </div>
-                    
-                ))}
-        </div>       
+                    <div key={index} className={chat.Sender +" message"}> {chat.Text} </div> 
+                    ))}
+            <div className='bottom-block'></div>
+        
+        </ReactScrollableFeed>
+            </div>
         </div>
 
-        <div className="columns mt-5 is-centered ">
-            <form className='column is-half'>
-                <div className='field'>
-                    <label className='label'>Text Me!</label>
-                    <div className="control">
-                        <input 
-                        type="text" 
-                        className="input" 
-                        onChange={getText}
-                        placeholder='Chat here'/>
-                    </div>
-                </div>    
-                <div className="field">
-                    <button onClick={saveChat} type='submit' className="button is-success">
+        <div className="write-chat">
+            <textarea 
+            type="text" 
+            className="input-area" 
+            onChange={getText}
+            onKeyDown = {keyDown}
+            placeholder='Chat here'
+            defaultValue={""}
+            value={Text}
+            />
+            <div className="field">
+                    <button onClick={saveChat} type='submit' className="button-send" id='btnSend'>
                         Send
                     </button>
-                </div>
-            </form>
+            </div>
         </div>
+        
+
+        </section>
         </div>
     )
 }
