676d8aaafde967b61fc4a1300405f2231870cb17
Genvictus <johannstarkmk42@gmail.com>
2023-05-05 21:57:58 +0700

    rfc: comments and returns

--- backend/controllers/ChatRegex.js
+++ backend/controllers/ChatRegex.js
@@ -2,55 +2,97 @@ import { evaluateExpression } from "./arithmetic";
 
 
 // REGEX for tambah pertanyaan
-const tambahPertanyaan = /Tambah(kan|in)? pertanyaan .* dengan jawaban .*( dong)?/i;
+const tambahPertanyaan = /Tambah(kan|in)? pertanyaan .* dengan jawaban .*( dong SMMCSBD)?/i
+const tambahPertanyaanPrefix = / *Tambah(kan|in)? pertanyaan */i
+const tambahPertanyaanInfix = / *dengan jawaban */i
+const tambahPertanyaanPostfix = / * dong SMMCSBD */i
 
 // REGEX for hapus pertanyaan
-const hapusPertanyaan = /Hapus pertanyaan .*( pl(ea|i)?se?| dong)?/i;
+const hapusPertanyaan = /Hapus pertanyaan .*( (pl(ea|i)?se?|dong) SMMCSBD)?/i;
+const hapusPertanyaanPrefix = / *Hapus pertanyaan */i
+const hapusPertanyaanPostfix = / *(pl(ea|i)?se?|dong) SMMCSBD */i
 
 // REGEX for tanggal queries
 const tanggal = /([0-2]?[0-9]|3[0-1])\/(0?[0-9]|1[0-2])\/[0-9]{1,4}/
 const tanyaTanggal1 = /Hari apa(kah)? ([0-2]?[0-9]|3[0-1])\/(0?[0-9]|1[0-2])\/[0-9]{1,4}\??/i
 const tanyaTanggal2 = /([0-2]?[0-9]|3[0-1])\/(0?[0-9]|1[0-2])\/[0-9]{1,4}( Hari apa\??)?/i
+const tanyaTanggalQuery = / *Hari apa(kah)?\?? */i
 
 // REGEX for calculator
 const kalkulator = /([0-9]|\+|\-|\*|\/|\^|\(|\)| )*/
 
+/**
+ * get the basic response of the bot based on the specified RegEx(es)
+ * @param {string} exp 
+ * @returns 
+ */
 export function respondMessage(exp) {
     if(tambahPertanyaan.test(exp)) {
         // parse & masukkan pertanyaan & jawaban ke db
+        let filteredExp = filterQuery(exp, [tambahPertanyaanPrefix, tambahPertanyaanPostfix]);
+        let query = filteredExp.split(tambahPertanyaanInfix);
+        // insert to db: query[0] -> pertanyaan & query[1] -> jawaban
+        // REMINDER: kalau pertanyaan sudah ada di query, jawaban baru update jawaban lama
+        return query[0] + " telah ditambahkan ke dalam daftar pertanyaan dengan jawaban " + query[1];
     }
     else if(hapusPertanyaan.test(exp)) {
         // parse & hapus pertanyaan dari db
+        let filteredExp = filterQuery(exp, [hapusPertanyaanPrefix, hapusPertanyaanPostfix]);
+        // delete from db: filteredExp -> pertanyaan
+        return filteredExp + " telah dihapus dari daftar pertanyaan";
     }
     else if(tanyaTanggal1.test(exp)
         || tanyaTanggal2.test(exp)) {
-        return "Tanggal " + exp + " jatuh pada hari " + getDayZeller(exp);
+        let filteredExp = filterQuery(exp, [tanyaTanggalQuery]);
+        return "Tanggal " + filteredExp + " jatuh pada hari " + getDayZeller(filteredExp);
     }
     else if(kalkulator.test(exp)) {
-        evaluateExpression(exp);
         return "Hasil dari ekspresi adalah " + evaluateExpression(exp);
     }
+    return null;
 }
 
-const HARI = {
-    0: "Minggu",
-    1: "Senin",
-    2: "Selasa",
-    3: "Rabu",
-    4: "Kamis",
-    5: "Jumat",
-    6: "Sabtu"
+/**
+ * Return removed specified RegEx(es) from an expression
+ * @param {string} exp 
+ * @param {RegExp[]} regExFilters 
+ * @returns 
+ */
+function filterQuery(exp, regExFilters) {
+    let filteredExp = exp;
+    regExFilters.forEach(filter => {
+        filteredExp = filteredExp.replace(filter, '');
+    });
+    return filteredExp
 }
+
+const HARI = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
 
 const MONTHLENGTH = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
+/**
+ * Get the day of a specified date in DD/MM/YYYY format
+ * @param {string} dateString 
+ * @returns 
+ */
 function getDayZeller(dateString) {
     // uses Zeller's Rule. Source: https://beginnersbook.com/2013/04/calculating-day-given-date/
     let datesNumber = parseDate(dateString);
-    const K = datesNumber[0];
-    const M = datesNumber[1];
-    const D = datesNumber[2]%100;
-    const C = Math.trunc(datesNumber[2]/100);
+    let K = datesNumber[0];
+    let M = datesNumber[1];
+    let D = datesNumber[2]%100;
+    let C = Math.trunc(datesNumber[2]/100);
+    /*
+    According to Zeller’s rule the month is counted as follows:
+    March is 1, April is 2….. January is 11 and February is 12.
+    So the year starts from March and ends with February. So if the given date has month as January or February subtract 1 from the year. For example:
+    For 1st January 1998 subtract 1 from 1998 i.e. 1998-1=1997 and use 1997 for calculating D.
+    Discard all the decimal values and then find the final value of F.
+    */
+    // subtract 1 from the year if the month is January or February
+    if(M <= 2) D -= 1;
+    // get month number, with March shifted as the starting month
+    M = ((M-3) + 12) % 12 + 1;
     /* 
     F=k+ [(13*m-1)/5] +D+ [D/4] +[C/4]-2*C
     k is  the day of the month.
@@ -58,14 +100,16 @@ function getDayZeller(dateString) {
     D is the last two digits of the year.
     C is the first two digits of the year.
     */
+
    let F = K + Math.trunc((13*M-1)/5) + D + Math.trunc(D/4) + Math.trunc(C/4) - 2*C
+   F %= 7;
 
    return HARI[F];
 }
 
 /**
  * format tanggal: DD/MM/YYYY
- * @param {*} dateString 
+ * @param {string} dateString 
  * @returns 
  */
 function parseDate(dateString) {
@@ -96,7 +140,7 @@ function parseDate(dateString) {
     return [day, month, year];
 }
 
-class InvalidDateError {
+class InvalidDateError extends Error {
     constructor() {
         super("Tanggal tidak valid!");
     }
